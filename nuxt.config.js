a5e0506263efcba58b903f875dc0ece00ee445e1
@s.okulov <okulov@grechka.digital>
2021-06-30 23:44:23 +0300

    Откат конфига

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,11 +64,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-	// publicPath: '/nuxt/'
-  },
-  router: {
-	  base:'./',
-	  mode: 'hash'
-  }
+  build: {}
 }
